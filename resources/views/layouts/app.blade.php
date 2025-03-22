47a6b2d06a5ad51baab6687ba7d2d4287615d19b
somtola <somtola.smt@gmail.com>
2025-03-22 15:43:55 +0700

    new update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,26 +19,87 @@
         @fluxAppearance
             @livewireStyles
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body class="min-h-screen bg-white dark:bg-zinc-800">
+        <flux:sidebar sticky stashable class="bg-zinc-50 dark:bg-zinc-900 border-r rtl:border-r-0 rtl:border-l border-zinc-200 dark:border-zinc-700">
+            <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+            <flux:brand href="{{route('dashboard')}}" logo="https://fluxui.dev/img/demo/logo.png" name="{{ Auth::user()->name }}" class="px-2 dark:hidden" />
+            <flux:brand href="{{route('dashboard')}}" logo="https://fluxui.dev/img/demo/dark-mode-logo.png" name="{{ Auth::user()->name }}" class="px-2 hidden dark:flex" />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-            @fluxScripts
-            @livewireScripts
+            <flux:input as="button" variant="filled" placeholder="Search..." icon="magnifying-glass" />
+
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="home" href="{{route('dashboard')}}" current>Home</flux:navlist.item>
+                <flux:navlist.item icon="inbox" badge="12" href="#">Inbox</flux:navlist.item>
+                <flux:navlist.item icon="document-text" href="#">Documents</flux:navlist.item>
+                <flux:navlist.item icon="calendar" href="#">Calendar</flux:navlist.item>
+
+                <flux:navlist.group expandable heading="Favorites" class="hidden lg:grid">
+                    <flux:navlist.item href="#">Marketing site</flux:navlist.item>
+                    <flux:navlist.item href="#">Android app</flux:navlist.item>
+                    <flux:navlist.item href="#">Brand guidelines</flux:navlist.item>
+                </flux:navlist.group>
+            </flux:navlist>
+
+            <flux:spacer />
+
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="cog-6-tooth" href="{{route('profile.edit')}}">Settings</flux:navlist.item>
+            </flux:navlist>
+
+            <flux:dropdown position="top" align="start" class="max-lg:hidden">
+                <flux:profile avatar="https://fluxui.dev/img/demo/user.png" name="{{ Auth::user()->name }}" />
+
+                <flux:menu>
+                    <flux:menu.radio.group>
+                        <flux:menu.radio checked>{{ Auth::user()->name }}</flux:menu.radio>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-dropdown-link :href="route('logout')"
+                                         onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                            <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
+                        </x-dropdown-link>
+                    </form>
+                </flux:menu>
+            </flux:dropdown>
+        </flux:sidebar>
+
+        <flux:header class="lg:hidden">
+            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+
+            <flux:spacer />
+
+            <flux:dropdown position="top" alignt="start">
+                <flux:profile avatar="https://fluxui.dev/img/demo/user.png" />
+
+                <flux:menu>
+                    <flux:menu.radio.group>
+                        <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
+                        <flux:menu.radio>Truly Delta</flux:menu.radio>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+
+                    <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
+                </flux:menu>
+            </flux:dropdown>
+        </flux:header>
+
+        <flux:main>
+            <flux:heading size="xl" level="1">Good afternoon, {{ Auth::user()->name }}</flux:heading>
+
+            <flux:text class="mb-6 mt-2 text-base">Here's what's new today</flux:text>
+
+            <flux:separator variant="subtle" />
+
+            {{ $slot }}
+        </flux:main>
+
+        @fluxScripts
     </body>
-
 </html>
